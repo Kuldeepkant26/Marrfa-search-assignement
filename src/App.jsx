eeedfe8689bb619eb928ee77ea3ce842378b49b1
Kuldeepkant26 <picasuplayers@gmail.com>
2024-12-05 22:57:43 +0530

    Fixed nav spellling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Components/Home'
-import Nav from './Components/nav'
+import Nav from './Components/Nav'
 import About from './Components/About'
 
 function App() {
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 function About() {
     return (
-        <div className='w-full flex items-center'>
+        <div className='w-full flex items-center flex-col gap-4'>
             <a className='text-blue-500 m-auto border-blue-500 border-[1px] p-2 rounded-md' href="https://main--ephemeral-cannoli-a899f8.netlify.app/" target='_blank'><i className="ri-user-fill"></i>Portfolio</a>
-            <a className='text-blue-500 m-auto border-blue-500 border-[1px] p-2 rounded-md' href="https://github.com/Kuldeepkant26/Marrfa-search-assignement" target='_blank'><i class="ri-github-fill"></i>Code</a>
+          
+            <a className='text-blue-500 m-auto border-blue-500 border-[1px] p-2 rounded-md' href="https://github.com/Kuldeepkant26/Marrfa-search-assignement" target='_blank'><i className="ri-github-fill text-2xl"></i>Code</a>
         </div>
     )
 }
